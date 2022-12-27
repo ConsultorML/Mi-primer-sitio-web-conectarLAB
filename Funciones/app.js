a0dd3d20406300a9fa78393247279c83056e295d
LucasZarandon <118391354+LucasZarandon@users.noreply.github.com>
2022-12-27 01:02:28 -0300

    Add files via upload

---
+++ Funciones/app.js
@@ -0,0 +1,5 @@
+
+function precionarInterruptor() {
+  document.querySelector(".menu-hamburguesa-nav").classList.toggle('visible');
+  document.querySelector(".menu-hamburguesa-icono").classList.toggle('fa-bars-staggered');
+}
